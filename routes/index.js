e57000125d25249bcd3c6792284d8786286aa981
LoGaNsF <logansf@gmail.com>
2015-06-19 17:40:30 +0200

    primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var quizController = require('../controllers/QuizController');
+
+/* Home */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+/* Quizes */
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
