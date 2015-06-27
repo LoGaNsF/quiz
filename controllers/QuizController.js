217e889dd175dc50e289eff9d4c29e3ecc0be063
LoGaNsF <logansf@gmail.com>
2015-06-27 19:13:20 +0200

    despliegue DB en Heroku

--- controllers/QuizController.js
+++ controllers/QuizController.js
@@ -1,13 +1,46 @@
-// Get /quizes/question
-exports.question = function(req, res) {
-	res.render('quizes/question', {question: 'Capital de Italia'});
+var models = require('../models/models');
+
+// Autoload
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.findById(quizId).then(function(quiz) {
+		if (quiz) {
+			req.quiz = quiz;
+			next();
+		} else {
+			next(new Error('No existe el quiz ' + quizId));
+		}
+	}).catch(function(error) {
+		next(error);
+	});
 };
 
-// Get /quizes/answer
-exports.answer = function(req, res) {
-	if (req.query.answer === 'Roma') {
-		res.render('quizes/answer', {answer: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {answer: 'Incorrecto'});
+// Get /quizes
+exports.index = function(req, res) {
+	var options = {};
+	if(req.query.search){
+		var search = '%' + req.query.search.replace(' ', '%') + '%';
+		options.where = {question: {$like: search}}
 	}
+
+	models.Quiz.findAll(options).then(function(quizes) {
+		res.render('quizes/index', {quizes: quizes});
+	})
+	.catch(function(error) {
+		next(error);
+	});
+};
+
+// Get /quizes/:id
+exports.show = function(req, res) {
+	res.render('quizes/show', {quiz: req.quiz});
+};
+
+// Get /quizes/:id/answer
+exports.answer = function(req, res) {
+	var result = 'Incorrecto';
+	if (req.query.answer === req.quiz.answer) {
+		result = 'Correcto';
+	}
+
+	res.render('quizes/answer', {quiz: req.quiz, answer: result});
 };
