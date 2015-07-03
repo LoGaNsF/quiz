3f69b844d09ebca3a3aefb7f34bdbae4125fbece
LoGaNsF <logansf@gmail.com>
2015-07-03 20:55:28 +0200

    editar preguntas

--- controllers/QuizController.js
+++ controllers/QuizController.js
@@ -47,11 +47,7 @@ exports.answer = function(req, res) {
 
 // GET /quizes/new
 exports.new = function(req, res) {
-	var quiz = models.Quiz.build({
-		question: 'Pregunta',
-		answer: 'Respuesta',
-		theme: 'otro'
-	});
+	var quiz = models.Quiz.build();
 
 	res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -65,6 +61,30 @@ exports.create = function(req, res) {
 			res.render('quizes/new', {quiz: quiz, errors: err.errors})
 		} else {
 			quiz.save({fields: ["question", "answer", "theme"]}).then(function() {
+				res.redirect('/quizes');
+			});
+		}
+	});
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz;
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.question = req.body.quiz.question;
+	req.quiz.answer = req.body.quiz.answer;
+	req.quiz.theme = req.body.quiz.theme;
+
+	req.quiz.validate().then(function(err) {
+		if (err) {
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors})
+		} else {
+			req.quiz.save({fields: ["question", "answer", "theme"]}).then(function() {
 				res.redirect('/quizes');
 			});
 		}
